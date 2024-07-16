9042c4fccca4715fc6ec19a704ec91f2abcf7295
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2024-07-17 00:10:18 +0200

    [Task] Kafka Base Config

---
+++ KafkaTemplate/src/main/java/org/Personal/Producer/Producer.java
@@ -0,0 +1,20 @@
+package org.Personal.Producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Producer {
+       private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public Producer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send("test-topic", message);
+        System.out.println("Sent message: " + message);
+    }
+}
