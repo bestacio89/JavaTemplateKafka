c103db9575df83ee8c83121a4e98b8edc04952df
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2024-08-09 23:17:07 +0200

    [Test] application context load test for error isolation

---
+++ KafkaTemplate/src/test/java/Integration/ApplicationContextTest.java
@@ -0,0 +1,16 @@
+package Integration;
+
+import Config.KafkaTestConfig;
+import org.Personal.Main;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(classes = {KafkaTestConfig.class, Main.class})
+@ActiveProfiles("test")
+public class ApplicationContextTest {
+    @Test
+    void contextLoads() {
+        // This test will automatically pass if the application context is loaded successfully
+    }
+}
