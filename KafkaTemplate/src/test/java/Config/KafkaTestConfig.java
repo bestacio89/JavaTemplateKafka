38c44d1901e6cb7c4a33fe30f1451b51c180f221
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2024-08-09 18:15:48 +0200

    [Fix]  KafkaTesting config

--- KafkaTemplate/src/test/java/Config/KafkaTestConfig.java
+++ KafkaTemplate/src/test/java/Config/KafkaTestConfig.java
@@ -4,8 +4,6 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.*;
@@ -15,7 +13,7 @@ import org.springframework.kafka.listener.MessageListener;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
-
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,15 +21,15 @@ import java.util.concurrent.CountDownLatch;
 
 @Configuration
 @EmbeddedKafka(partitions = 1, topics = {"test-topic"})
+@ActiveProfiles("test")
 public class KafkaTestConfig {
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
     }
 
     @Bean
-    @Qualifier("kafkaProducerFactory")
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
@@ -41,7 +39,6 @@ public class KafkaTestConfig {
     }
 
     @Bean
-    @Qualifier("kafkaConsumerFactory")
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
