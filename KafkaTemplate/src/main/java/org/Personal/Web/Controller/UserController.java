36b0b710f627ec48bbc47a66c4a59b5136592b96
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2024-08-21 10:40:16 +0200

    Multiple api and docker fixes.

--- KafkaTemplate/src/main/java/org/Personal/Web/Controller/UserController.java
+++ KafkaTemplate/src/main/java/org/Personal/Web/Controller/UserController.java
@@ -2,7 +2,10 @@ package org.Personal.Web.Controller;
 
 import org.Personal.Domain.Postgres.BusinessObjects.User;
 import org.Personal.Service.UserService;
+import org.Personal.Web.Controller.Models.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,12 +16,30 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public User createUser(@RequestBody User user) {
-        return userService.createUser(user);
+    public ResponseEntity<ApiResponse<User>> createUser(@RequestBody User user) {
+        try {
+            User createdUser = userService.createUser(user);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(new ApiResponse<>(HttpStatus.CREATED.value(), "User created successfully", createdUser));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Failed to create user", null));
+        }
     }
 
     @GetMapping("/{username}")
-    public User getUserByUsername(@PathVariable String username) {
-        return userService.getUserByUsername(username);
+    public ResponseEntity<ApiResponse<User>> getUserByUsername(@PathVariable String username) {
+        try {
+            User user = userService.getUserByUsername(username);
+            if (user != null) {
+                return ResponseEntity.ok(new ApiResponse<>(HttpStatus.OK.value(), "User found", user));
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body(new ApiResponse<>(HttpStatus.NOT_FOUND.value(), "User not found", null));
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Error retrieving user", null));
+        }
     }
 }
